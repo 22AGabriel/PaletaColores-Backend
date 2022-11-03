65471eba3224b99d294554ad5425273b96da7c5d
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-11-03 20:22:28 -0300

    agregué los middlewares y ruta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express"
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 
 const app = express();
 app.set("port", process.env.PORT || 4003)
@@ -7,4 +10,14 @@ app.listen(app.get("port"), ()=>{
     console.log("estoy en el puerto " + app.get("port"))
 })
 
-console.log("Paleta de colores")
+// Midlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extends:true}));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'../public')));
+
+// Rutas
+app.get("apipaletacolores", (req, res)=>{
+    res.send('Esto es una prueba de una peticion get')
+})
